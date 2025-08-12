d6d4587e11145403afb70708d3e414e2dc0eaf22
JunkCross <alangokuface19@gmail.com>
2025-08-11 23:22:13 -0600

    pequeña modificacion

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigosSorteo = [];
-let numeroSorteado = 0;
-
 
 function agregarAmigo(){
     let amigo = document.getElementById('amigo').value;
